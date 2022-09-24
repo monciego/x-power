41327e48f1058a52fbe1eaf66009dfb59e748601
Jericho <jerichobantiquete@gmail.com>
2022-09-25 00:15:19 +0800

    dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,8 +54,7 @@
         </aside>
         <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
             {{-- Header --}}
-            {{-- @include('components.header') --}}
-            header
+            @include('components.header')
             {{-- Contents --}}
             <main>
                 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
