380c1a369c2b7b3bfb81b830bf82f52b6129e8aa
Jericho <jerichobantiquete@gmail.com>
2022-09-29 00:19:58 +0800

    user dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-[#161616] fixed left-0 right-0 text-white border-b border-gray-700">
+<nav x-data="{ open: false }" class="bg-[#161616] fixed left-0 right-0 text-white border-b border-gray-700 z-[800]">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -1,15 +1,37 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('User Dashboard') }}
-        </h2>
-    </x-slot> --}}
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-[#1a1f23] overflow-hidden shadow-sm text-white sm:rounded-lg">
-                <div class="p-6 bg-[#1a1f23] border-b border-gray-900">
-                    User Dashboard
+    <div class="py-10 px-2 sm:px-4 lg:px-8">
+        <h2 class="text-2xl font-bold mb-6 text-white">Discover</h2>
+        <div class="flex gap-4 md:flex-nowrap flex-wrap">
+            <div class="h-[30rem] w-full rounded-lg shadow-md bg-[#1a1f23] p-4 relative">
+                <div class=" h-[60%]">
+                    <img src="https://images.pexels.com/photos/4857734/pexels-photo-4857734.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        alt="" class="rounded-lg h-full w-full object-cover">
+                </div>
+                <div class="h-[40%]">
+                    <p class="font-medium mt-4 text-gray-100">Our Service</p>
+                    <h2 class="text-2xl md:text-3xl font-bold mr-auto cursor-pointer text-gray-200">
+                        Break downs won’t break you down anymore.
+                    </h2>
+                    <button
+                        class="my-4 transition ease-in duration-300 inline-flex items-center text-sm font-medium  bg-purple-700 px-5 py-2 hover:shadow-lg tracking-wider text-white rounded-md hover:bg-purple-600 ">
+                        <span>Explore our services</span>
+                    </button>
+                </div>
+            </div>
+            <div class="h-[30rem] w-full  rounded-lg shadow-md bg-[#1a1f23] p-4 relative">
+                <div class=" h-[60%]">
+                    <img src="https://images.pexels.com/photos/4846392/pexels-photo-4846392.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        alt="" class="rounded-lg h-full w-full object-cover">
+                </div>
+                <div class="h-[40%]">
+                    <p class="font-medium mt-4 text-gray-100">Our Products</p>
+                    <h2 class="text-2xl md:text-3xl font-bold mr-auto cursor-pointer text-gray-200">
+                        One-stop shopping for all your automotive needs.
+                    </h2>
+                    <button
+                        class="my-4 transition ease-in duration-300 inline-flex items-center text-sm font-medium mb-2 md:mb-0 bg-purple-700 px-5 py-2 hover:shadow-lg tracking-wider text-white rounded-md hover:bg-purple-600 ">
+                        <span>Explore our products</span>
+                    </button>
                 </div>
             </div>
         </div>
