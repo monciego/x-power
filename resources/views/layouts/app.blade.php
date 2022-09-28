adc55bf1a91b99b4a857a58006885fb7ea191817
Jericho <jerichobantiquete@gmail.com>
2022-09-28 22:19:00 +0800

    changed colors in user dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,15 +31,8 @@
         @if (Auth::user()->hasRole('user'))
         @include('layouts.navigation')
 
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-
         <!-- Page Content -->
-        <main>
+        <main class="pt-16">
             {{ $slot }}
         </main>
     </div>
